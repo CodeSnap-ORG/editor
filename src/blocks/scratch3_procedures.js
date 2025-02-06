6bf034c34a7bcbb81807b089314fd642382c1cce
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-06 17:44:30 -0600

    Update scratch3_procedures.js
    Added functionality for "Is UltiBlocks?" boolean

--- src/blocks/scratch3_procedures.js
+++ src/blocks/scratch3_procedures.js
@@ -125,6 +125,9 @@ class Scratch3ProcedureBlocks {
             if (lowercaseValue === 'is turbowarp?') {
                 return true;
             }
+            if (lowercaseValue === 'is ultiblocks?') {
+                return true;
+            }
             // When the parameter is not found in the most recent procedure
             // call, the default is always 0.
             return 0;
