8e4f957ecbffde36a2dc51757966d74628af5887
aec <aec@noreply.codeberg.org>
2025-05-03 22:09:51 +0100

    how did i not push this? maybe this should help with the fix.

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -647,7 +647,7 @@ class VirtualMachine extends EventEmitter {
 
         return zip.generateAsync({
             type: typeof optZipType === 'string' ? optZipType : 'blob',
-            mimeType: 'application/x.scratch.sprite3',
+            mimeType: 'application/x.ampmod.asz',
             compression: 'DEFLATE',
             compressionOptions: {
                 level: 6
