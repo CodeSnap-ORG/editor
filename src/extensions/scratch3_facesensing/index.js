3567ed26e3530cb9c420a5be22c64f83d1c38c16
8to16 <8to16@noreply.codeberg.org>
2025-08-25 23:24:19 +0200

    scratch3_facesensing stub
    Scratch will add facesensing extension later this year.
    This exists merely as a stub to prepare for it.
    The prop

---
+++ src/extensions/scratch3_facesensing/index.js
@@ -0,0 +1,3 @@
+// Made in prep for autumn 2025.
+// Placeholder until this can be properly implemented.
+// Keep in mind that there may be legal issues due to mixing TurboWarp's MPL code and Scratch's AGPL code.
