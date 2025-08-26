8f1fb62a54e35b90d08197e5696425027a51b6b3
8to16 <ampelectrecuted@gmail.com>
2025-08-26 19:31:42 +0100

    Mostly completed webpack 5 migration (does npm install, but is broken)

--- webpack.config.js
+++ webpack.config.js
@@ -33,9 +33,7 @@ const base = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     devtool: process.env.SOURCEMAP || (process.env.NODE_ENV === 'production' ? false : 'cheap-module-source-map'),
     devServer: {
-        contentBase: path.resolve(__dirname, 'build'),
         host: '0.0.0.0',
-        disableHostCheck: true,
         compress: true,
         port: process.env.PORT || 8601,
         // allows ROUTING_STYLE=wildcard to work properly
@@ -68,7 +66,7 @@ const base = {
     },
     module: {
         rules: [{
-            test: /\.jsx?$/,
+            test: /\jsx?$/,
             loader: 'babel-loader',
             include: [
                 path.resolve(__dirname, 'src'),
@@ -103,20 +101,19 @@ const base = {
             }, {
                 loader: 'postcss-loader',
                 options: {
-                    ident: 'postcss',
-                    plugins: function () {
-                        return [
+                    postcssOptions: {
+                        plugins: [
                             postcssImport,
                             postcssVars,
                             autoprefixer
-                        ];
+                        ]
                     }
                 }
             }]
         },
         {
             test: /\.(svg|png|wav|mp3|gif|jpg|woff2|hex)$/,
-            loader: 'url-loader',
+            loader: 'asset/source',
             options: {
                 limit: 8192, // Convert images < 8kb to base64 strings
                 outputPath: 'static/assets/',
@@ -279,7 +276,7 @@ module.exports = [
                 rules: base.module.rules.concat([
                     {
                         test: /\.(svg|png|wav|mp3|gif|jpg|woff2|hex)$/,
-                        loader: 'url-loader',
+                        loader: 'asset/source',
                         options: {
                             limit: 2048,
                             outputPath: 'static/assets/',
