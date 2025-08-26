3fca44c9dd3670b6af9697afca30814f4028da5b
8to16 <ampelectrecuted@gmail.com>
2025-08-26 11:34:37 +0100

    Remove IE check Internet Explorer is long gone. Who would use AmpMod on it in 2025? Scratch doesn't support IE either so I'm not sure why they check for that.

--- src/lib/audio/shared-audio-context.js
+++ src/lib/audio/shared-audio-context.js
@@ -1,29 +1,26 @@
 import StartAudioContext from '@turbowarp/startaudiocontext';
-import * as bowser from 'bowser';
 import log from '../log';
 
 let AUDIO_CONTEXT;
 
-if (bowser.parse(navigator.userAgent).name !== 'Internet Explorer') {
-    /**
-     * AudioContext can be initialized only when user interaction event happens
-     */
-    const event =
-        typeof document.ontouchstart === 'undefined' ?
-            'mousedown' :
-            'touchstart';
-    const initAudioContext = () => {
-        document.removeEventListener(event, initAudioContext);
+/**
+ * AudioContext can be initialized only when user interaction event happens
+ */
+const event =
+    typeof document.ontouchstart === 'undefined' ?
+        'mousedown' :
+        'touchstart';
+const initAudioContext = () => {
+    document.removeEventListener(event, initAudioContext);
 
-        try {
-            AUDIO_CONTEXT = new (window.AudioContext || window.webkitAudioContext)();
-            StartAudioContext(AUDIO_CONTEXT);
-        } catch (e) {
-            log.error('could not create shared audio context; sound-related features will not be available', e);
-        }
-    };
-    document.addEventListener(event, initAudioContext);
-}
+    try {
+        AUDIO_CONTEXT = new (window.AudioContext || window.webkitAudioContext)();
+        StartAudioContext(AUDIO_CONTEXT);
+    } catch (e) {
+        log.error('could not create shared audio context; sound-related features will not be available', e);
+    }
+};
+document.addEventListener(event, initAudioContext);
 
 /**
  * Wrap browser AudioContext because we shouldn't create more than one
