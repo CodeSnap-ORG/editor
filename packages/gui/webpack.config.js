140b13afe197ee608518496d6522b9490beb56fe
8to16 <ampelectrecuted@gmail.com>
2025-09-02 20:51:07 +0100

    chore(gui): actually get the version

--- packages/gui/webpack.config.js
+++ packages/gui/webpack.config.js
@@ -27,7 +27,7 @@ const htmlWebpackPluginCommon = {
 };
 
 // When this changes, the path for all JS files will change, bypassing any HTTP caches
-const CACHE_EPOCH = `amp-${process.env.ampmod_version}`;
+const CACHE_EPOCH = `amp-${JSON.stringify(process.env.npm_package_version)}`;
 
 const base = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
