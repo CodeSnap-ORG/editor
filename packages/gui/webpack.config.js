f0eb5312b85bed4cde808b2724a4e655de91ffd9
8to16 <ampelectrecuted@gmail.com>
2025-09-06 17:11:27 +0100

    fix(gui): fix paths for files

--- packages/gui/webpack.config.js
+++ packages/gui/webpack.config.js
@@ -169,8 +169,8 @@ const base = {
         new CompressionPlugin({
             filename:
                 process.env.NODE_ENV === "production"
-                    ? `${CACHE_EPOCH}-brotli/[name].br`
-                    : "brotli/[name].br",
+                    ? `js/${CACHE_EPOCH}-brotli/[name].br`
+                    : "js/brotli/[name].br",
             algorithm: "brotliCompress",
             test: /js\/amp\-.*\.js$/,
             compressionOptions: {
