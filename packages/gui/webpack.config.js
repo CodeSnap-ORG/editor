4a8c0ff16f303552c46ca2b23faf68e4acea817c
8to16 <ampelectrecuted@gmail.com>
2025-09-06 17:17:39 +0100

    remove thing that is causing bugs

--- packages/gui/webpack.config.js
+++ packages/gui/webpack.config.js
@@ -169,8 +169,8 @@ const base = {
         new CompressionPlugin({
             filename:
                 process.env.NODE_ENV === "production"
-                    ? `js/${CACHE_EPOCH}-brotli/[name].br`
-                    : "js/brotli/[name].br",
+                    ? `js/${CACHE_EPOCH}/[name].br`
+                    : "js/[name].br",
             algorithm: "brotliCompress",
             test: /js\/amp\-.*\.js$/,
             compressionOptions: {
