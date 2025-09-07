2fb8283eac99317ca1dd65bd9d61aead430184b8
8to16 <ampelectrecuted@gmail.com>
2025-09-07 11:54:57 +0100

    fix(gui): fix app_name when accessing from webpack

--- packages/gui/src/lib/brand.js
+++ packages/gui/src/lib/brand.js
@@ -1,5 +1,8 @@
 // Legacy export format because this is used by some build-time scripts stuck in the past.
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-    APP_NAME: process.env.ampmod_is_canary ? "AmpMod (canary build)" : "AmpMod",
+    APP_NAME:
+        process.env.ampmod_is_canary || process.env.BUILD_MODE == "canary"
+            ? "AmpMod (canary build)"
+            : "AmpMod",
 };
