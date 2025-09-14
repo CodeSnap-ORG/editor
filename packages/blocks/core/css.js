287dc142910a23da8b29705fafa194c367ae7559
8to16 <ampelectrecuted@gmail.com>
2025-09-14 22:22:30 +0100

    feat: use Inter Variable Inter Variable has practically infinite font weights which are closer to what was once when Helvetica was used.

--- packages/blocks/core/css.js
+++ packages/blocks/core/css.js
@@ -228,7 +228,7 @@ Blockly.Css.CONTENT = [
     'box-shadow: 4px 4px 20px 1px rgba(0,0,0,.15);',
     'color: #000;',
     'display: none;',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 9pt;',
     'opacity: 0.9;',
     'padding: 2px;',
@@ -313,7 +313,7 @@ Blockly.Css.CONTENT = [
     'border: 1px solid $colour_numPadBorder;',
     'cursor: pointer;',
     'font-weight: 600;',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 12pt;',
     '-webkit-tap-highlight-color: rgba(0,0,0,0);',
   '}',
@@ -356,7 +356,7 @@ Blockly.Css.CONTENT = [
     '-webkit-user-select: text;',
     '-ms-user-select: text;',
     'text-align: center;',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: .8em;',
   '}',
 
@@ -452,7 +452,7 @@ Blockly.Css.CONTENT = [
 
   '.blocklyText {',
     'fill: $colour_text;',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 12pt;',
     'font-weight: 500;',
   '}',
@@ -523,7 +523,7 @@ Blockly.Css.CONTENT = [
   '}',
 
   '.blocklyFlyoutLabelText {',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 14pt;',
     'fill: $colour_flyoutLabelColor;',
     'font-weight: bold;',
@@ -669,7 +669,7 @@ Blockly.Css.CONTENT = [
 
   '.scratchCommentText {',
     'color: black;',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 12pt;',
     'font-weight: 400;',
   '}',
@@ -699,7 +699,7 @@ Blockly.Css.CONTENT = [
 
   '.blocklyHtmlInput {',
     'border: none;',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 12pt;',
     'height: 100%;',
     'margin: 0;',
@@ -844,7 +844,7 @@ Blockly.Css.CONTENT = [
     'overflow-x: visible;',
     'overflow-y: auto;',
     'position: absolute;',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'z-index: 40;', /* so blocks go over toolbox when dragging */
     '-webkit-tap-highlight-color: transparent;', /* issue #1345 */
   '}',
@@ -934,7 +934,7 @@ Blockly.Css.CONTENT = [
 
   '.blocklyTreeLabel {',
     'cursor: default;',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 16px;',
     'padding: 0 3px;',
     'vertical-align: middle;',
@@ -982,7 +982,7 @@ Blockly.Css.CONTENT = [
   '}',
 
   '.scratchColourPickerLabel {',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 0.65rem;',
     'color: $colour_valueReportForeground;',
     'margin: 8px;',
@@ -1003,7 +1003,7 @@ Blockly.Css.CONTENT = [
   '}',
 
   '.scratchNotePickerKeyLabel {',
-    'font-family: Inter, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Inter Variable", "Helvetica Neue", Helvetica, Arial, sans-serif;',
     'font-size: 0.75rem;',
     'fill: $colour_blackText;',
     'pointer-events: none;',
--- packages/gui/src/playground/import-first.js
+++ packages/gui/src/playground/import-first.js
@@ -1,4 +1,4 @@
 import "./public-path";
 import "../lib/tw-polyfill";
 import "modern-normalize"; // amp: Use a better normalize stylesheet for modern browsers
-import "@fontsource/inter"; //amp: Use a different font
+import "@fontsource-variable/inter"; //amp: Use a different font
