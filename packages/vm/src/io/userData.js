00f821a6e4cfe62a39bc9c3062729ab62ab28271
kRxZy_kRxZy <krxzy_krxzy@noreply.codeberg.org>
2025-09-17 21:17:43 +0200

    fix this to include all userdata funcs

--- packages/vm/src/io/userData.js
+++ packages/vm/src/io/userData.js
@@ -1,5 +1,33 @@
-class UserData { 
-  constructor() { this._username = ''; } 
-  async fetchUsername() { this._username = (await (await fetch('https://ampmod-api.onrender.com/session', { credentials: 'include' })).json()).username; } 
-} 
+class UserData {
+    constructor() {
+        this._username = "";
+    }
+
+    /**
+     * Fetch the username from the remote API and store it locally
+     */
+    async fetchUsername() {
+        const response = await fetch('https://ampmod-api.onrender.com/session', { credentials: 'include' });
+        const data = await response.json();
+        this._username = data.username;
+    }
+
+    /**
+     * Handler for updating the username
+     * @param {object} data Data posted to this ioDevice.
+     * @property {!string} username The new username.
+     */
+    postData(data) {
+        this._username = data.username;
+    }
+
+    /**
+     * Getter for username. Initially empty string, until set via postData or fetchUsername.
+     * @returns {!string} The current username
+     */
+    getUsername() {
+        return this._username;
+    }
+}
+
 module.exports = UserData;
