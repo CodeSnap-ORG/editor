fccb8c04df532028a9c2b4c96a74043b63fd253b
8to16 <ampelectrecuted@gmail.com>
2025-09-20 19:50:48 +0100

    chore(gui): redo the fun facts list from scratch Some actually useful tips were copied over. No jokes.

--- packages/gui/src/lib/amp-fun-facts.js
+++ packages/gui/src/lib/amp-fun-facts.js
@@ -1,45 +1,16 @@
 /*
- * This file is under a moratorium due to most previous contributions focusing on it. See:
- * https://codeberg.org/ampmod/ampmod/issues/4
- *
- * If you really want to edit this, you should be contributing to more useful parts of the code
- * before contributing to this file.
+ * The fun fact system has been repurposed for tips.
+ * Jokes are no longer allowed here.
+ * A lot of previous tips have been removed because of this.
  */
 
 export const funFacts = [
-    "Fun fact: AmpMod is currently loading.",
-    "Did you know? In AmpMod, you can put variables inside of boolean inputs.",
-    "Fun fact: AmpMod was previously known as UltiBlocks.",
-    "I LOVE TURBOWARP!",
-    "I LOVE SCRATCH!",
-    "I LOVE SCRATCH 2.0!",
-    "YOU can contribute to AmpMod!",
-    "Fun fa-Sadly, this fact was eaten by an evil kumquat.",
-    "Fun fact: qwertyuiopasdfghjklzxcvbnm",
+    "Tip: You can put variables inside of boolean and array inputs.",
     "Did you know? There is an AmpMod wiki where you can find information about AmpMod.",
     "Did you know? The AmpMod Manual has information for new AmpMod users.",
-    "Did you know? The Witch sprite was originally going to be the AmpMod mascot",
-    "'How to make a computer in AmpMod' Oh wait, this isn't Google, is it?",
-    "Fun fact: The person who typed this fact had too much caffeine!",
-    "Tip: You said to press ANYTHING while doing a keyboard smash! What do you mean 7 is wrong?!",
-    "Fun fact: We are cool",
-    "How to AmpMod 101: Step 1: AmpMod.",
-    "Fun fact: AmpMod should be finished on exactly Februtembober 60th, 29908.",
-    "April Fool's- not. :D",
-    "Fun fact: There were 2 AmpMod topics on the Scratch forums, but one was dustbinned.",
-    "Fun fact: AmpMod used to be hosted on GitHub, but is now hosted exclusively on Codeberg.",
-    "Fun fact: AmpMod is 100% free and open source software.",
-    "Preparing emojis...",
-    "Herding cats...",
-    "Growing apples...",
-    "Removing dangos...",
-    "Modifying features...",
-    "Converting legacy lists...",
-    "Breaking everything... Okay, fine, I`m kidding!",
-    "Preparing the AmpMods",
-    "There is a canary version of AmpMod if you want to have the latest features in exchange for bugs.",
-    "bleh",
-    "Oh noes an error. Just kidding! :D",
-    "Super AmpMod Bros. Real 2019",
-    "abc.xyz is the website of Google's subsidiary.",
+    "Fun fact: AmpMod is 100% free and open source software, and you can contribute on Codeberg.",
+    "Fun fact: Over 250 people use AmpMod every week, and this number is growing every day.",
+    "Fun fact: AmpMod has over 100 extensions and over 135 blocks.",
+    'Remember: "if () was matched, then" blocks must be in the "test if () was matched" block!',
+    "Tip: You can create custom reporters in AmpMod.",
 ];
