8825a1627dc0cb4467db07e7470d35754545b1df
8to16 <ampelectrecuted@gmail.com>
2025-09-21 12:31:36 +0100

    chore(vm): change platform details temporarily

--- packages/vm/src/engine/tw-platform.js
+++ packages/vm/src/engine/tw-platform.js
@@ -2,9 +2,10 @@
 // This can be accessed externally on `vm.runtime.platform`
 
 module.exports = {
-    name: "AmpMod",
+    // TODO: amp: change this back to just AmpMod
+    name: "AmpMod (EXPERIMENTAL COMPILER)",
     url: "https://ampmod.codeberg.page/",
     // amp: added extra information about platform
     version: process.env.ampmod_version,
-    contact: "https://ampmod.flarum.cloud",
+    contact: "https://codeberg.org/ampmod/ampmod/issues",
 };
