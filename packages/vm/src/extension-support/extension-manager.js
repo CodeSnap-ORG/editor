8d9da5ecb772dd722397b6bf9108fadb9cef77b9
8to16 <ampelectrecuted@gmail.com>
2025-09-22 20:12:27 +0100

    chore(vm): remove tw extension again just like pre-rewrite, this is for cleanup and separation of concerns.

--- packages/vm/src/extension-support/extension-manager.js
+++ packages/vm/src/extension-support/extension-manager.js
@@ -25,8 +25,6 @@ const defaultBuiltinExtensions = {
     makeymakey: () => require("../extensions/scratch3_makeymakey"),
     boost: () => require("../extensions/scratch3_boost"),
     gdxfor: () => require("../extensions/scratch3_gdx_for"),
-    // tw: core extension
-    tw: () => require("../extensions/tw"),
 };
 
 /**
