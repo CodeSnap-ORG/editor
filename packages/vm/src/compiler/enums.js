4335cd056e8d883d42a0f5b0d188c58ebca2a60a
8to16 <ampelectrecuted@gmail.com>
2025-09-24 15:53:42 +0100

    feat(vm): add `ARRAY` InputType

--- packages/vm/src/compiler/enums.js
+++ packages/vm/src/compiler/enums.js
@@ -88,6 +88,9 @@ const InputType = {
 
     /** An array of values in the form [R, G, B] */
     COLOR: 0x2000,
+
+    /** amp: An array. */
+    ARRAY: 0xffff,
 };
 
 /**
@@ -289,6 +292,8 @@ const InputOpcode = {
     PROCEDURE_ARGUMENT: "procedures.argument",
 
     CONTROL_COUNTER: "control.counter",
+
+    ARRAYS_DELIMITED: "arrays.delimited",
 
     TW_KEY_LAST_PRESSED: "tw.lastKeyPressed",
 };
