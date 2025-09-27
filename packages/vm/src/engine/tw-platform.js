ec965b8fa38929bfcd44685ca98ecc0767c0db51
8to16 <ampelectrecuted@gmail.com>
2025-09-27 20:21:19 +0100

    chore(vm): remove "EXPERIMENTAL COMPILER" from platform name Not so "experimental" anymore

--- packages/vm/src/engine/tw-platform.js
+++ packages/vm/src/engine/tw-platform.js
@@ -2,8 +2,7 @@
 // This can be accessed externally on `vm.runtime.platform`
 
 module.exports = {
-    // TODO: amp: change this back to just AmpMod
-    name: "AmpMod (EXPERIMENTAL COMPILER)",
+    name: "AmpMod",
     url: "https://ampmod.codeberg.page/",
     // amp: added extra information about platform
     version: process.env.ampmod_version,
