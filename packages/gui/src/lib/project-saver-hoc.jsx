edec869511d4d66fa103143612554d017b3b221f
8to16 <ampelectrecuted@gmail.com>
2025-10-03 17:02:08 +0100

    Revert "Migrate componentWillMount"
    This reverts commit f5f807c05e72fbd0cb75c0a5f69bc152190fbe18.

--- packages/gui/src/lib/project-saver-hoc.jsx
+++ packages/gui/src/lib/project-saver-hoc.jsx
@@ -50,7 +50,7 @@ const ProjectSaverHOC = function (WrappedComponent) {
                 "tryToAutoSave",
             ]);
         }
-        componentDidMount() {
+        componentWillMount() {
             if (typeof window === "object") {
                 // Note: it might be better to use a listener instead of assigning onbeforeunload;
                 // but then it'd be hard to turn this listening off in our tests
