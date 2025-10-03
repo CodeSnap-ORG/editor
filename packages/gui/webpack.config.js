8d0683998696e7445b93ed2b05b3496c4df78a8d
8to16 <ampelectrecuted@gmail.com>
2025-10-03 21:25:53 +0100

    fix(gui): fix non-production builds failing

--- packages/gui/webpack.config.js
+++ packages/gui/webpack.config.js
@@ -301,7 +301,7 @@ module.exports = [
                           }),
                           new OptimizeCSSAssetsPlugin({}),
                       ]
-                    : false,
+                    : [],
         },
         plugins: base.plugins.concat([
             new webpack.DefinePlugin({
