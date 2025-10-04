146f71689d7b7196df38baec325c821557bf2503
8to16 <ampelectrecuted@gmail.com>
2025-10-04 16:37:12 +0100

    chore: integrate `editor-extra-keys` into core, and remove it

--- packages/gui/src/addons/generated/addon-entries.js
+++ packages/gui/src/addons/generated/addon-entries.js
@@ -56,7 +56,6 @@ export default {
   "paint-by-default": () => import(/* webpackChunkName: "addon-entry-paint-by-default" */ "../addons/paint-by-default/_runtime_entry.js"),
   "block-cherry-picking": () => import(/* webpackChunkName: "addon-default-entry" */ "../addons/block-cherry-picking/_runtime_entry.js"),
   "hide-new-variables": () => import(/* webpackChunkName: "addon-entry-hide-new-variables" */ "../addons/hide-new-variables/_runtime_entry.js"),
-  "editor-extra-keys": () => import(/* webpackChunkName: "addon-entry-editor-extra-keys" */ "../addons/editor-extra-keys/_runtime_entry.js"),
   "hide-delete-button": () => import(/* webpackChunkName: "addon-entry-hide-delete-button" */ "../addons/hide-delete-button/_runtime_entry.js"),
   "no-script-bumping": () => import(/* webpackChunkName: "addon-entry-no-script-bumping" */ "../addons/no-script-bumping/_runtime_entry.js"),
   "disable-stage-drag-select": () => import(/* webpackChunkName: "addon-entry-disable-stage-drag-select" */ "../addons/disable-stage-drag-select/_runtime_entry.js"),
